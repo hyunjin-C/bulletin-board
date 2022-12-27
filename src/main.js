dd90486c74055c74df45baac4b6d0a514282a999
hyunjin-C <zn9852@naver.com>
2022-12-28 04:20:56 +0900

    style: 로그인된 메인페이지 레이아웃 제작

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-function goMain() {
+// 로그인이 잘 되었는지 확인하는 로직 추가
+// 로그아웃이 잘 되었는지 확인하는 로직 추가
+// 로그아웃하면 '로그아웃 되었습니다.' 메시지 팝업 띄우기
+// 회원가입하면 '회원가입을 축하합니다.' 메시지 팝업 띄우기
+
+function goHome() {
   let link = "../index.html";
+  location.href = link;
+}
+
+function goMain() {
+  let link = "./mainPage.html";
+  location.href = link;
+}
+
+function goMypage() {
+  let link = "./myPage.html";
+  location.href = link;
+}
+
+function goLogin() {
+  let link = "./pages/login.html";
   location.href = link;
 }
 
